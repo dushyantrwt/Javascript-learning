1f73e1c2ab824c0c762543b048cd1247826b0ab1
dushyantrwt <dushyantrawat150@gmail.com>
2025-06-17 20:19:12 +0530

    initial coomit

---
+++ script.js
@@ -0,0 +1,7 @@
+const product ={
+    name: "BAll Pen",
+    price: 270,
+    offer:5,
+    description: "A ballpoint pen with a sleek design and smooth writing experience.",
+};
+console.log(product["name"]);
